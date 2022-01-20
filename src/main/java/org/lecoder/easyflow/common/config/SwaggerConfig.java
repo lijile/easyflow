5a7bc28a0e09021ab082e9667671f1001adaa2f2
lijile <lijile-gd@cpic.com.cn>
2022-01-20 11:18:57 +0800

    升级swagger3

--- src/main/java/org/lecoder/easyflow/common/config/SwaggerConfig.java
+++ src/main/java/org/lecoder/easyflow/common/config/SwaggerConfig.java
@@ -1,20 +1,18 @@
 package org.lecoder.easyflow.common.config;
 
+import org.lecoder.easyflow.EasyflowApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.ApiKey;
-import springfox.documentation.service.AuthorizationScope;
-import springfox.documentation.service.SecurityReference;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -25,17 +23,16 @@ import java.util.List;
  * @date: 2021/10/25 14:00
  * @version: 1.0
  */
-@Configuration
-@EnableSwagger2
 @Profile({"test"})
+@Configuration
 public class SwaggerConfig {
 
     @Bean
     public Docket createRestApi() {
-        return new Docket(DocumentationType.SWAGGER_2)
+        return new Docket(DocumentationType.OAS_30)
                 .apiInfo(apiInfo())
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("org.lecoder.easyflow"))
+                .apis(RequestHandlerSelectors.basePackage(EasyflowApplication.class.getPackage().getName()))
                 .paths(PathSelectors.any())
                 .build()
                 .securitySchemes(securitySchemes())
@@ -50,8 +47,10 @@ public class SwaggerConfig {
                 .build();
     }
 
-    private List<ApiKey> securitySchemes() {
-        return Arrays.asList(new ApiKey("Authorization", "Authorization", "header"));
+    private List<SecurityScheme> securitySchemes() {
+        List<SecurityScheme> securitySchemeList = new ArrayList<>();
+        securitySchemeList.add(new ApiKey("Authorization", "Authorization", "header"));
+        return securitySchemeList;
     }
 
     private List<SecurityContext> securityContexts() {
--- src/main/java/org/lecoder/easyflow/common/config/WebConfig.java
+++ src/main/java/org/lecoder/easyflow/common/config/WebConfig.java
@@ -2,6 +2,7 @@ package org.lecoder.easyflow.common.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
@@ -20,6 +21,7 @@ public class WebConfig {
      * @return
      */
     @Bean
+    @Profile({"test"})
     public CorsFilter corsFilter() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
         corsConfiguration.addAllowedOrigin("*");
